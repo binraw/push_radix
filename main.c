48ef160d43c26a084d1f85edb24ea78d23b31481
binraw <truvelotrobin@yahoo.fr>
2024-03-09 18:27:47 +0100

    fix size

--- main.c
+++ main.c
@@ -60,7 +60,7 @@ void	control_sort_list(t_list **stack_a, t_list **stack_b)
 {
 	t_stacks *s;
 
-	s = malloc(sizeof * s);
+	s = malloc(ft_lstsize(*stack_a) * sizeof *s);
 	if (ft_lstsize(*stack_a) == 2)
 	{
 		if ((*stack_a)->content > (*stack_a)->next->content)
